7806331218c41be65d9dfb6ac6f8cdbfd2f5fae8
Ilia <ilia.trofimov2001@icloud.com>
2021-05-06 21:04:20 +0500

    Added lagrange, function that return Lagrange polynomial for given data set. Changed signature of poly_inter.

--- Approx.py
+++ Approx.py
@@ -1,28 +1,31 @@
 import numpy as np
-import matplotlib
+from matplotlib import pyplot as plt
 
+def poly_inter(x_data, y_data):
+    matr_a = np.zeros((len(x_data), len(x_data)), dtype=float)
+    vect_b = np.zeros((len(x_data), 1), dtype=float)
 
-def poly_inter(data: np.ndarray):
-    matr_a = np.zeros((len(data), len(data)), dtype=float)
-    vect_b = np.zeros((len(data), 1))
-
-    for i in range(len(data)):
-        vect_b[i] = np.sum(data[:, 1] * data[:, 0]**i)
-        matr_a[i] = np.array([np.sum(data[:, 0]**(i+j)) for j in range(len(data))])
+    for i in range(len(x_data)):
+        vect_b[i] = np.sum(y_data * x_data**i)
+        matr_a[i] = np.array([np.sum(x_data**(i+j)) for j in range(len(x_data))])
 
     vect_a = np.linalg.solve(matr_a, vect_b)
-    polynom = f"lambda x: {vect_a[0,0]}"
+    result = f"lambda x:{vect_a[0,0]:f}"
     for i in range(1, len(vect_a)):
-        polynom += f"+{vect_a[i, 0]}*x**{i}"
+        result += f"+{vect_a[i, 0]:f}*x**{i}"
 
-    return eval(polynom)
-
-
-data = [[1950, 155], [1960, 210], [1970, 285], [1980, 375], [1990, 510], [2000, 650]]
-poly = poly_inter(data)
-
-data.append([2010, poly(2010)])
-data.append([2020, poly(2020)])
+    return eval(result)
 
 
+def lagrange(x_data, y_data):
+    result = f"lambda x:"
 
+    for i in range(len(x_data)):
+        coef = y_data[i]
+        for j in range(len(x_data)):
+            if j != i:
+                coef /= (x_data[i] - x_data[j])
+                result += f"(x-{x_data[j]})*"
+        result += f"{coef} + "
+
+    return eval(result[:len(result) - 2])
