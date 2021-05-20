4d9f16e059d2e287585780daf3e69a01c5935e69
IliaTrofimov <ilia.trofimov2001@icloud.com>
2021-05-21 01:12:51 +0300

    Spline derivative in Xi is equal to f'(xi)

--- Approx.py
+++ Approx.py
@@ -2,42 +2,17 @@ import numpy as np
 from matplotlib import pyplot as plt
 
 
-def f(x):
-    return np.cos(3 * x) / x
+def ls_polynomial(n, x_data, y_data):
+    if len(x_data) != len(y_data):
+        raise ValueError("x_data and y_data sizes do not match!")
 
-
-def test(n):
-    dataX = np.linspace(0.1, 10, n)
-    dataY = f(dataX)
-
-    poly1 = lagrange(dataX, dataY)
-    poly2 = least_squares(dataX, dataY)
-
-    _, ax = plt.subplots(figsize=(10, 6))
-    ax.scatter(dataX, f(dataX))
-    ax.scatter(dataX, poly1(dataX), color="red")
-    ax.scatter(dataX, poly2(dataX), color="green")
-
-    dataX = np.linspace(0.1, 10, n * 20)
-
-    ax.plot(dataX, f(dataX), label="f")
-    ax.plot(dataX, poly1(dataX), label=f"lagrange{n}", color="red")
-    ax.plot(dataX, poly2(dataX), label=f"polynomial{n}", color="green")
-
-    ax.grid()
-    ax.set_ylim([-2, 2])
-    ax.legend()
-    plt.show()
-
-
-def least_squares(n, x_data, y_data):
-    matr_a = np.zeros((n, n), dtype=float)
+    matr = np.zeros((n, n), dtype=float)
     vect_b = np.zeros(n, dtype=float)
 
     for i in range(n):
         vect_b[i] = np.sum(y_data * x_data ** i)
-        matr_a[i] = np.array([np.sum(x_data ** (i + j)) for j in range(n)])
-    vect_a = np.linalg.solve(matr_a, vect_b)
+        matr[i] = np.array([np.sum(x_data ** (i + j)) for j in range(n)])
+    vect_a = np.linalg.solve(matr, vect_b)
     result = np.polynomial.polynomial.Polynomial(vect_a)
 
     def p(x):
@@ -46,6 +21,9 @@ def least_squares(n, x_data, y_data):
 
 
 def lagrange(x_data, y_data):
+    if len(x_data) != len(y_data):
+        raise ValueError("x_data and y_data sizes do not match!")
+
     def p(x):
         total = 0.0
         n = len(x_data)
@@ -69,24 +47,24 @@ def nearest(val: float, arr: list):
     return arr[k]
 
 
-def sqr_spline(x_data, y_data, dy_data=None):
+def sqr_spline(x_data, y_data, dy=None):
     if len(x_data) != len(y_data):
         raise ValueError("x_data and y_data sizes do not match!")
 
     x_data = np.asarray(x_data)
     y_data = np.asarray(y_data)
 
-    if dy_data is None:
-        dy_data = np.zeros_like(y_data)
-
-
     matrix = np.array([
         [1, x_data[0], x_data[0] ** 2],
         [1, x_data[1], x_data[1] ** 2],
-        [0, 1, x_data[0] ** 2]
+        [1, x_data[2], x_data[2] ** 2]
     ], dtype=float)
+    b_vect = np.array([y_data[0], y_data[1], y_data[2]], dtype=float)
 
-    b_vect = np.array([y_data[0], y_data[1], dy_data[0]], dtype=float)
+    if dy is not None:
+        matrix[2] = [0, 1, 2*x_data[0]]
+        b_vect[2] = dy
+
     polynomials = {x_data[0]: np.polynomial.Polynomial(np.linalg.solve(matrix, b_vect))}
     matrix[2, 0] = 0
     matrix[2, 1] = 1
@@ -94,8 +72,9 @@ def sqr_spline(x_data, y_data, dy_data=None):
     for i in range(1, len(x_data) - 1):
         matrix[:2, 1:] = [[x_data[i], x_data[i] ** 2],
                           [x_data[i+1], x_data[i+1] ** 2]]
-        matrix[2, 2] = 2 * x_data[i]
-        b_vect = np.array([y_data[i], y_data[i + 1], dy_data[i]])
+        matrix[2, 2] = 2*x_data[i]
+
+        b_vect = np.array([y_data[i], y_data[i + 1], polynomials[x_data[i-1]].deriv()(x_data[i])])
         polynomials[x_data[i]] = np.polynomial.Polynomial(np.linalg.solve(matrix, b_vect))
 
     def p(x):
@@ -109,17 +88,4 @@ def sqr_spline(x_data, y_data, dy_data=None):
                 key = nearest(x[j], keys)
                 result[j] = polynomials[key](x[j])
             return result
-
-    def dp(x):
-        keys = list(polynomials.keys())
-        if isinstance(x, (float, int)):
-            key = nearest(x, keys)
-            return polynomials[key].deriv()(x)
-        else:
-            result = np.zeros_like(x)
-            for j in range(len(x)):
-                key = nearest(x[j], keys)
-                result[j] = polynomials[key].deriv()(x[j])
-            return result
-    return p, dp
-
+    return p
